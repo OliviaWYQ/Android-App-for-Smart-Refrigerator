6b0a3bbbedc759da58d0d9be0de44c8e113b2a7c
OliviaWYQ <295199218@qq.com>
2019-03-13 18:30:23 -0400

    add anychart api

--- SmartFridge/app/src/main/java/com/example/ls/smartfridge/MainActivity.java
+++ SmartFridge/app/src/main/java/com/example/ls/smartfridge/MainActivity.java
@@ -3,11 +3,32 @@ package com.example.ls.smartfridge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.DataEntry;
+import com.anychart.Pie;
+import com.anychart.ValueDataEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+
+        pie.data(data);
+
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+        anyChartView.setChart(pie);
     }
 }
